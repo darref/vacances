2d51ad59d35a8d20a1564eeb7f47a71b8a812b79
Darref <jordan.ferrad@gmail.com>
2023-08-29 23:04:20 +0200

    plus ou moins

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,34 @@ function palindrome(chaine:string)
   }
 }
 
+function nombreAleatoire(max:number) {
+  return Math.floor(Math.random() * max);
+}
+
+function plusOuMoins(nombreATrouver:number, maxEssais:number) {
+  let nbCoups = 0;
+
+  while (nbCoups < maxEssais) {
+    let nombreEssai = nombreAleatoire(10);
+
+    if (nombreEssai === nombreATrouver) {
+      console.log("Trouvé en " + (nbCoups + 1) + " coups ! ! !");
+      return;
+    } else if (nombreEssai > nombreATrouver) {
+      console.log("Moins");
+    } else {
+      console.log("Plus");
+    }
+
+    nbCoups++;
+  }
+
+  console.log("Nombre non trouvé en " + maxEssais + " coups . . . ");
+}
+
+
+
 //console.log(factorielle(100));
-console.log(palindrome("kayak"));
-console.log(palindrome("maison"));
+//console.log(palindrome("kayak"));
+//console.log(palindrome("maison"));
+plusOuMoins(8 , 20000);
